e327e459059b9b83db95e3f4582035a984a4db42
Christophe VG <contact@christophe.vg>
2022-09-18 17:57:04 +0200

    fixed test

--- tests/test_step.py
+++ tests/test_step.py
@@ -7,14 +7,14 @@ from testman import Step, Assertion
 def test_basic_operation():
   def f():
     return True
-  step = Step("name", f, asserts=[ Assertion("result == True")] )
+  step = Step(name="name", func=f, asserts=[ Assertion("result == True")] )
   result = step.execute()
   assert result == True
 
 def test_basic_failing_assertion():
   def f():
     return True
-  step = Step("name", f, asserts=[ Assertion("result == False")] )
+  step = Step(name="name", func=f, asserts=[ Assertion("result == False")] )
   success = True
   try:
     step.execute()
